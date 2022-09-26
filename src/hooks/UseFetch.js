4fcbfcedd9a29487dedc92604aa2ed8b00aed2f6
mrota89 <marcello.rota.1989@gmail.com>
2022-09-26 22:26:27 +0200

    rivisto sistema di ricerca città

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import axios from 'axios';
 import { API_KEY, API_BASE_URL } from '../api/config'
 
-const useFetch = (firstUrl, secondUrl) => {
+const useFetch = () => {
   const [weatherResponse, setWeatherResponse] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -15,27 +15,35 @@ const useFetch = (firstUrl, secondUrl) => {
       //ottengo longitudine e latitudine della città ricercata
       const cityGeoCoding = await axios.get(`${API_BASE_URL}/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
       const dataCityGeoCoding = cityGeoCoding["data"][0];
-      const lat = dataCityGeoCoding['lat'];
-      const lon = dataCityGeoCoding['lon'];
-      const country = dataCityGeoCoding['country'];
-      const localNames = dataCityGeoCoding['local_names'];
-      const cityName = ('local_names' in dataCityGeoCoding && 'it' in localNames)?localNames['it']:dataCityGeoCoding['name'];
       
-      const weatherData = await axios.get(`${API_BASE_URL}data/3.0/onecall?lat=${lat}&lon=${lon}&lang=it&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY}`);
-      weatherData['data']['current'].country = country;
-      weatherData['data']['current'].name = cityName;
-      setLoading(false);
+      if(dataCityGeoCoding) {
 
-      //salvo messaggio per mostrarlo all'utente nella error page 
-      if (weatherData['data']['message'] >= 400 || weatherData['data']['message'] >= 500) {
-        setError(weatherData['data']['message']);
+        const lat = dataCityGeoCoding['lat'];
+        const lon = dataCityGeoCoding['lon'];
+        const country = dataCityGeoCoding['country'];
+        const localNames = dataCityGeoCoding['local_names'];
+        const cityName = ('local_names' in dataCityGeoCoding && 'it' in localNames)?localNames['it']:dataCityGeoCoding['name'];
+        const weatherData = await axios.get(`${API_BASE_URL}data/3.0/onecall?lat=${lat}&lon=${lon}&lang=it&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY}`);
+        
+        if(cityName.toUpperCase() === city.toUpperCase()) {
+          weatherData['data']['current'].country = country;
+          weatherData['data']['current'].name = cityName;
+          setLoading(false);
+          setWeatherResponse(weatherData);
+
+        } else {
+          setLoading(false);
+          return alert([`Attenzione! Forse intendevi questa località: ${cityName.toUpperCase()}`]);
+        }
+
+      } else {
+        setLoading(false);
+        return alert(['Attenzione! La località inserita non esiste!']);
       }
-  
-      setWeatherResponse(weatherData);
    
     } catch (error) {
       setLoading(false);
-      setError(error);
+      setError(true);
     }
   }, []);
 
