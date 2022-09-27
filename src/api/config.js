8638f36adaf1dc0151fe6275129b5b0aa9280d6e
mrota89 <73886110+mrota89@users.noreply.github.com>
2022-09-27 12:58:47 +0200

    Update config.js
    tolta api key

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const API_KEY = 'ce97289d8e043c9a246af4d9d5700874';
+export const API_KEY = ''; //inserire qui la propria API KEY ottenuta da openweathermap.org
 export const API_BASE_URL = 'http://api.openweathermap.org/';
